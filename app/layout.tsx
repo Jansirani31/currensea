be936b35c6769c136957b0a83a329809bb0bdd05
Jansirani <jansiraniramesh31@gmail.com>
2026-02-19 22:32:42 +0530

    Moved favicon to public and updated layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "CurrenSea",
   description: "Next Gen OTC Aggregation",
+  icons: {
+    icon: "/favicon.png",
+  },
 };
 
 export default function RootLayout({
